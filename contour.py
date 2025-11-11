3958780d49b8f7f31e30924f0377a1f1dbe5687a
sone030813-glitch <sone030813@gmail.com>
2025-11-11 21:49:06 +0000

    Postcode_level_all_meters_electricity

---
+++ contour.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+path = ''
+pd = pd.read_csv(path)
+
+"""
+1, i need to transfer postcode to longitude and altitude
+2, use gaussion to smooth data
+3, build a coordinate system
+4, aggregation data
+5, smooth data by gaussion
+6, build heat map and contour
+"""
+
